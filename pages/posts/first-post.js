e1a2f9e7100100f67f2c90654283f30389876227
unknown <kimitawanjohi7923@gmail.com>
2021-07-09 17:42:17 +0300

    Image component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,15 +1,20 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
 
 export default function FirstPost() {
 
     return (
         <div>
+            <Head>
+                <title>this is the first post</title>
+            </Head>
             <Link href="/">
                 <a>home</a>
             </Link>
             <h2> This is the first post</h2>
             <div>
-                <img src="images/profile.jpg" alt="kimita" />
+                <Image src="/images/profile.jpg" width={144} height={144} alt="kimita" />
             </div>
         </div>
     )
